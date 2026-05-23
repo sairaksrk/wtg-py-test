100a0a915f7e81d574df92cb20feec189f7ef019
siwakorn <siwakorn.swkk@gmail.com>
2026-05-24 05:27:52 +0700

    fix compensation-table ui

--- components/features/compensation/compensationManagement/compensation-request-detail.tsx
+++ components/features/compensation/compensationManagement/compensation-request-detail.tsx
@@ -27,75 +27,96 @@ import {
 } from "@/types/compensation";
 import { getPageSize } from "@/utils/helpers";
 import { PersonnelSearchModal } from "./personnel-search-modal";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/utils/helpers";
 
-interface SummaryCardProps {
+interface AllocationCardProps {
   title: string;
   salary: string;
   percent: string;
-  amount: string;
+  allocated: string;
+  spent: string;
+  balance: string;
 }
 
-const SummaryCard = ({ title, salary, percent, amount }: SummaryCardProps) => (
-  <Card className="border shadow-none rounded-[20px]">
-    <CardContent className="p-5">
-      <h4 className="text-sm font-normal text-[#18181B] mb-4">{title}</h4>
-      <div className="space-y-3">
+const AllocationCard = ({
+  title,
+  salary,
+  percent,
+  allocated,
+  spent,
+  balance,
+}: AllocationCardProps) => (
+  <Card className="shadow-none rounded-[20px] min-w-[320px] max-w-[320px] flex-1 bg-white overflow-hidden flex flex-col justify-between">
+    <CardContent className="p-5 pb-0">
+      <h4
+        className="text-sm font-medium text-[#18181B] mb-4 truncate"
+        title={title}
+      >
+        {title}
+      </h4>
+      <div className="space-y-3 text-sm">
         <div className="flex justify-between items-center">
-          <span className="text-sm text-subdude">เงินเดือน</span>
-          <span className="text-sm font-normal text-[#18181B]">{salary}</span>
+          <span className="text-subdude">เงินเดือนปัจจุบันรวม</span>
+          <span className="font-normal text-[#18181B]">{salary}</span>
         </div>
         <div className="flex justify-between items-center">
-          <span className="text-sm text-subdude">จัดสรร (ร้อยละ)</span>
-          <span className="text-sm font-normal text-[#18181B]">{percent}</span>
+          <span className="text-subdude">จัดสรรร้อยละ (%)</span>
+          <span className="font-normal text-[#18181B]">{percent}</span>
         </div>
         <div className="flex justify-between items-center">
-          <span className="text-sm text-subdude">วงเงิน</span>
-          <span className="text-sm font-normal text-[#18181B]">{amount}</span>
+          <span className="text-subdude">วงเงินจัดสรร</span>
+          <span className="font-normal text-[#18181B]">{allocated}</span>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-subdude">ใช้ไป</span>
+          <span className="font-normal text-[#18181B]">{spent}</span>
         </div>
       </div>
     </CardContent>
+    <div className="px-3 text-sm">
+      <div className="px-4 mt-4 bg-[#F0F7FF] w-full flex justify-between items-center rounded-[6px]">
+        <span className="text-subdude">คงเหลือ</span>
+        <span className="font-normal text-[#18181B]">{balance}</span>
+      </div>
+    </div>
   </Card>
 );
 
-const MOCKUP_SUMMARY_CARDS: any[] = [
+const MOCKUP_ALLOCATION_CARDS: AllocationCardProps[] = [
   {
-    title: "ที่ปรึกษาฯ",
-    salary: "76,800",
-    percent: "3.00%",
-    amount: "2,304.00",
+    title: "กลุ่มชำนาญการพิเศษลงมา (ผอ.สบน.)",
+    salary: "4,308,240.00",
+    percent: "3.00/0.25/0.15",
+    allocated: "8,693.24",
+    spent: "0.00",
+    balance: "8,693.24",
   },
   {
-    title: "รองผู้อำนวยการ",
-    salary: "134,690",
-    percent: "3.00%",
-    amount: "4,040.70",
+    title: "กลุ่มเชี่ยวชาญ (ผอ.สบน.)",
+    salary: "372,250.00",
+    percent: "0.25/0.15",
+    allocated: "720.36",
+    spent: "0.00",
+    balance: "720.36",
   },
   {
-    title: "ผู้อำนวยการสำนัก",
-    salary: "465,720",
-    percent: "3.00/0.25%",
-    amount: "2,581.60",
+    title: "กลุ่มรองผู้อำนวยการ (ผอ.สบน.)",
+    salary: "134,690.00",
+    percent: "3.00",
+    allocated: "4,040.70",
+    spent: "0.00",
+    balance: "4,040.70",
   },
   {
-    title: "ผู้เชี่ยวชาญ",
-    salary: "372,250",
-    percent: "0.25/0.15%",
-    amount: "720.36",
-  },
-  {
-    title: "ชำนาญการพิเศษลงมา",
-    salary: "4,308,240",
-    percent: "3.00/0.25/0.15%",
-    amount: "2,581.61",
+    title: "กลุ่มที่ปรึกษา (ผอ.สบน.)",
+    salary: "76,800.00",
+    percent: "3.00",
+    allocated: "2,304.00",
+    spent: "0.00",
+    balance: "2,304.00",
   },
 ];
-
-const MOCKUP_SUMMARY_DATA: any = {
-  salary: "76,800.00",
-  allocated: "2,304.00",
-  spent: "0.00",
-  balance: "2,304.00",
-};
 
 const MOCKUP_PAGINATION: any = {
   totalPersonnel: 5,
@@ -257,6 +278,19 @@ export default function CompensationRequestDetail({
     setPersonnelData(updatedData);
   }, []);
 
+  const onSubmit = async () => {
+    alert.fire({
+      type: "warning",
+      title: "ยืนยันการพิจารณา",
+      description: "โปรดตรวจสอบความถูกต้องของข้อมูล",
+      confirmButton: {
+        label: c("button.confirm"),
+        onClick: () => toastSuccess(c("successfully")),
+      },
+      cancelButton: { label: c("button.secondary-cancel"), show: true },
+    });
+  };
+
   const [filters, setFilters] = useTableState<CompensationListParams>(
     COMPENSATION_REQUEST_SESSION_KEY,
     {
@@ -271,18 +305,6 @@ export default function CompensationRequestDetail({
     },
   );
 
-    // const { data, isLoading, isError, error } = useCompensationRequestsList({
-  //   page: filters.page,
-  //   take: filters.take,
-  //   requestNo: filters.requestNo,
-  //   name: filters.name,
-  //   startDate: filters.startDate,
-  //   positionId: filters.positionId,
-  //   positionLevelId: filters.positionLevelId,
-  //   departmentId: filters.departmentId,
-
-  // });
-  
   const onSearch = (formData: any) => {
     setFilters({
       ...filters,
@@ -313,6 +335,7 @@ export default function CompensationRequestDetail({
   return (
     <div className="min-h-screen bg-[#FAFAFA]">
       <div className="max-w-full mx-auto px-4">
+        {/* Back Button & Title */}
         <div className="flex items-center gap-4 mb-6 pt-2">
           <Button
             variant="secondary"
@@ -324,61 +347,90 @@ export default function CompensationRequestDetail({
           >
             <Icon icon="solar:alt-arrow-left-outline" className="text-base" />
           </Button>
-          <h1 className="text-xl font-medium text-[#18181B]">ที่ปรึกษาฯ</h1>
+          <h1 className="text-xl font-medium text-[#18181B]">ย้อนกลับ</h1>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mb-6">
-          {MOCKUP_SUMMARY_CARDS.map((card, index) => (
-            <SummaryCard
-              key={index}
-              title={card.title}
-              salary={card.salary}
-              percent={card.percent}
-              amount={card.amount}
-            />
-          ))}
-        </div>
-
-        <Card className="border rounded-[20px] mb-6 overflow-hidden">
-          <div className="overflow-x-auto ">
-            <CardContent className="p-5 min-w-max">
-              <h3 className="text-xl font-medium text-[#18181B] mb-8">
-                บริหารวงเงิน ที่ปรึกษา
-              </h3>
-
-              <div className="flex items-center justify-between gap-20">
-                <div className="flex gap-[306.5px]">
-                  <div className="flex flex-col gap-2">
-                    <p className="text-sm text-subdude">เงินเดือน</p>
-                    <p className="text-base font-normal text-[#18181B]">
-                      {MOCKUP_SUMMARY_DATA.salary}
-                    </p>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="text-sm text-subdude">เงินจัดสรร</p>
-                    <p className="text-base font-normal text-[#18181B]">
-                      {MOCKUP_SUMMARY_DATA.allocated}
-                    </p>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p className="text-sm text-subdude">เงินที่ได้เลื่อน</p>
-                    <p className="text-base font-normal text-[#18181B]">
-                      {MOCKUP_SUMMARY_DATA.spent}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="bg-[#F0F7FF] px-6 py-5 rounded-[20px] flex flex-col items-end min-w-[350px]">
-                  <p className="text-sm text-subdude mb-1">คงเหลือ</p>
-                  <p className="text-[28px] font-semibold text-[#18181B]">
-                    {MOCKUP_SUMMARY_DATA.balance}
-                  </p>
-                </div>
+        {/* Top Card: Group Name & Status */}
+        <Card className="shadow-none rounded-[20px] bg-white p-6 mb-6">
+          <div className="flex flex-col gap-2">
+            <span className="text-sm text-subdude">ชื่อกลุ่ม</span>
+            <h2 className="text-xl font-medium text-[#18181B]">
+              กลุ่มผอ.สำนัก (ผอ.สบน.)
+            </h2>
+            <div className="flex items-center gap-3 mt-1">
+              <Badge className="bg-[#FFF7ED] text-[#F97316] hover:bg-[#FFF7ED] border-none px-3 py-1 text-xs font-normal rounded-full flex items-center gap-1">
+                <span className="w-1.5 h-1.5 rounded-full bg-[#F97316]" />
+                รอพิจารณา
+              </Badge>
+              <div className="border-r border-gray-200 h-4" />
+              <div className="flex items-center gap-1.5 text-sm text-subdude">
+                <Icon icon="solar:user-linear" className="size-4" />
+                <span>ทรัพย์ธนิตา วิเชียรชาญ</span>
               </div>
-            </CardContent>
+            </div>
           </div>
         </Card>
 
+        {/* Main Card */}
+        <Card className="shadow-none rounded-[20px] bg-white overflow-hidden mb-6">
+          <CardContent className="p-6">
+            <div className="mb-6">
+              <h3 className="text-xl font-medium text-[#18181B]">
+                การจัดสรรวงเงิน
+              </h3>
+              <p className="text-sm text-subdude mt-1">
+                กลุ่มผอ.สำนัก (ผอ.สบน.)
+              </p>
+            </div>
+
+            <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-6">
+              <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 flex-1">
+                <div className="flex flex-col gap-1">
+                  <span className="text-sm text-subdude">
+                    เงินเดือนปัจจุบันรวม
+                  </span>
+                  <span className="text-base font-normal text-[#18181B]">
+                    463,720.00
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-sm text-subdude">จัดสรรร้อยละ (%)</span>
+                  <span className="text-base font-normal text-[#18181B]">
+                    0.25/3.00
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-sm text-subdude">วงเงินจัดสรร</span>
+                  <span className="text-base font-normal text-[#18181B]">
+                    2,581.60
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-sm text-subdude">ใช้ไป</span>
+                  <span className="text-base font-normal text-[#18181B]">
+                    0.00
+                  </span>
+                </div>
+              </div>
+
+              <div className="bg-[#F0F7FF] px-6 py-4 rounded-[20px] flex flex-col items-end min-w-70">
+                <span className="text-xs text-subdude mb-1">คงเหลือ</span>
+                <span className="text-2xl font-semibold text-[#18181B]">
+                  2,581.60
+                </span>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* Horizontal Scrollable Cards */}
+        <div className="flex overflow-x-auto gap-4 mb-6 scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-transparent">
+          {MOCKUP_ALLOCATION_CARDS.map((card, index) => (
+            <AllocationCard key={index} {...card} />
+          ))}
+        </div>
+
+        {/* Personnel List Table */}
         <Card className="border border-gray-100 shadow-none rounded-3xl bg-white overflow-hidden">
           <CardHeader className="flex flex-row items-center justify-between p-6 pb-4">
             <div className="flex flex-col gap-1">
@@ -439,6 +491,24 @@ export default function CompensationRequestDetail({
           </p>
         </div>
       </div>
+
+      {/* Sticky Footer */}
+      <footer className="rounded-full bg-white px-6 py-4 sticky bottom-0">
+        <div className="flex items-center justify-between gap-3">
+          <Button
+            variant="outline"
+            type="button"
+            className="bg-[#F4F4F5] border-[#F4F4F5] text-red-500 hover:text-red-500"
+            // onClick={() => onDeleteRequest?.(reqId)}
+            disabled={!reqId}
+          >
+            ลบรายการ
+          </Button>
+          <Button type="submit" onClick={onSubmit}>
+            พิจารณาเสร็จสิ้น
+          </Button>
+        </div>
+      </footer>
     </div>
   );
 }
